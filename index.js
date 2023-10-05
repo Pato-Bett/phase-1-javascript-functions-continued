c1e6b60503db78c65caf6036e2464fcd10cb802a
Pato-Bett <patobett588@gmail.com>
2023-10-05 12:24:52 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    let activityStatement = 'This Saturday, I want to ' + activity + '!'
+    return activityStatement
+}
+function mondayWork(activity = 'go to the office'){
+    let activityStatement = 'This Monday, I will ' + activity + '.'
+    return activityStatement
+}
+function wrapAdjective(wrapper){
+    let result = function(adjective){
+        return ('You are ' + wrapper + adjective + wrapper + '!')
+    }
+    return result
+}
